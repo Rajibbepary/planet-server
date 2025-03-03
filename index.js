74088095744755486c261b3d71f004894d6879dc
Rajibbepary <rajib635356@gmail.com>
2025-03-03 22:06:48 +0600

    delete route creat

--- index.js
+++ index.js
@@ -181,6 +181,15 @@ app.get('/customer-orders/:email', verifyToken, async(req, res)=>{
   res.send(result)
 })
 
+app.delete('/orders/:id', verifyToken, async(req, res)=>{
+  const id = req.params.id
+  const query ={_id: new ObjectId(id)}
+  const result = await ordersCollection.deleteOne(query)
+  res.send(result)
+})
+
+
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
